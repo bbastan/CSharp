fb28487a7ee10a274ea696c25a75eee723fa18d6
Babak Bastan <74910913+bbastan@users.noreply.github.com>
2023-10-29 15:00:16 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace exercise1_cw1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word: ");
+            string word1 = Console.ReadLine();
+            Console.WriteLine("Enter a other word: ");
+            string word2 = Console.ReadLine();
+            string result = (word1 == word2) ? "true" : "false";
+            Console.WriteLine(result);
+        }
+    }
+}
